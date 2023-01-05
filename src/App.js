022b629ccc216670b7a4a3ff24f48cc5d6a3490d
RAJKUMAR PALANICHAMY <rajkumar743cse@gmail.com>
2023-01-05 15:46:52 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
+import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
 import './App.css';
 
 function App() {
+  const [input,setInput] = useState();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  
+     <div class="App">
+      <textarea autoFocus class="textarea" value={input} onChange={
+          (e) => setInput(e.target.value)
+      }/>
+  <ReactMarkdown source={input} className="markdown"/>
+  <div>sbdhbc</div>
+     </div>
+
+  )
 }
 
-export default App;
+export default App
